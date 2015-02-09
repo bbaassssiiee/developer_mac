b166f732a42a495599beb35536d7dd5200514c97
Bas Meijer <bas.meijer@me.com>
2015-02-09 20:33:20 +0100

    reduce manual updates

--- install_on_my_mac.sh
+++ install_on_my_mac.sh
@@ -3,6 +3,13 @@
 ### @bbaassssiiee
 ### https://github.com/bbaassssiiee/developer_mac
 
+# Ask for the administrator password upfront
+sudo -v
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true;do sudo -n true;sleep 60;kill -0 "$$"||exit;done 2>/dev/null &
+
+softwareupdate -i -r -a
+
 ## download_apple_xcode: http://appstore.com
 # You need to download XCode, or only the command line tools
 xcrun -f git || (open https://developer.apple.com/downloads && exit 1)
--- update_my_mac.sh
+++ update_my_mac.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+softwareupdate -i -r -a
+brew update
 ## Ansible updates the Mac software using brew, cask & ansible
 ansible-playbook provisioner.yml -i inventory.ini --ask-sudo-pass
