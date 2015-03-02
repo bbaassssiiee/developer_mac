5bfb34fefdd8b0e017b1ba2ff02cd73ba8de3e84
Bas Meijer <bas.meijer@me.com>
2015-03-02 11:24:05 +0100

    Panamax makes Docker user-friendly

--- panamax.sh
+++ panamax.sh
@@ -2,4 +2,4 @@
 ##install_panamax:
 brew install --upgrade http://download.panamax.io/installer/brew/panamax.rb
 panamax init
-open http://10.0.0.200:3000/dashboard
+#open http://10.0.0.200:3000/dashboard
