a9f693b09818e005aabe3fe8ddfbcb8a594fac56
Bas Meijer <bas.meijer@me.com>
2015-03-16 10:51:58 +0100

    Munki see, Munki do!

--- update_my_mac.sh
+++ update_my_mac.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/bash -v
+# software updates could be done using Munki
 #softwareupdate -i -r -a
+
 brew update
 brew prune
 ## Ansible updates the Mac software using brew, cask & ansible
 ansible-playbook provisioner.yml -i inventory.ini --ask-sudo-pass
+
