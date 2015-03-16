8584a8b2792c90b0c52059a4e4b0c1913ac46b7c
Bas Meijer <bas.meijer@me.com>
2015-03-16 23:02:06 +0100

    Munki server setup automated

--- update_my_mac.sh
+++ update_my_mac.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -v
 # software updates could be done using Munki
-#softwareupdate -i -r -a
+softwareupdate -i -r -a
 
 brew update
+brew cleanup
 brew prune
+brew upgrade brew-cask || /usr/bin/true
+brew cask cleanup
+
 ## Ansible updates the Mac software using brew, cask & ansible
 ansible-playbook provisioner.yml -i inventory.ini --ask-sudo-pass
 
