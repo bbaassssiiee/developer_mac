13a2ce4e870fa2f272c2b57e68bc10e829fabb5a
Bas Meijer <bas.meijer@me.com>
2015-03-16 17:28:53 +0100

    generate the munki app-store with Ansible

---
+++ install_munkiserver.sh
@@ -0,0 +1,1 @@
+ansible-playbook -v --ask-sudo-pass -l osxserver server.yml
---
+++ remove_munki.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo launchctl unload /Library/LaunchDaemons/com.googlecode.munki.*
+
+sudo rm -rf "/Applications/Utilities/Managed Software Update.app"
+#Munki 2 only:
+sudo rm -rf "/Applications/Managed Software Center.app"
+
+sudo rm -f /Library/LaunchDaemons/com.googlecode.munki.*
+sudo rm -f /Library/LaunchAgents/com.googlecode.munki.*
+sudo rm -rf "/Library/Managed Installs"
+sudo rm -rf /usr/local/munki
+sudo rm /etc/paths.d/munki
+
+sudo pkgutil --forget com.googlecode.munki.admin
+sudo pkgutil --forget com.googlecode.munki.app
+sudo pkgutil --forget com.googlecode.munki.core
+sudo pkgutil --forget com.googlecode.munki.launchd
+
+# reset App Store to pull updates from default Apple servers
+sudo defaults delete /Library/Preferences/com.apple.SoftwareUpdate CatalogURL
+
