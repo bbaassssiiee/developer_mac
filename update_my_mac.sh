65887b37c7062a5a47ebf75e58796b46b045f66e
Bas Meijer <bas.meijer@me.com>
2015-05-21 08:13:54 +0200

    adding brew upgrade

--- update_my_mac.sh
+++ update_my_mac.sh
@@ -2,5 +2,6 @@
 softwareupdate -i -r -a
 brew update
 brew prune
+brew upgrade
 ## Ansible updates the Mac software using brew, cask & ansible
 ansible-playbook provisioner.yml -i inventory.ini --ask-sudo-pass
