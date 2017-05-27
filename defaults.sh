f6a8bfba11c63ba7b9482638e79e4a5aebc1cb35
Bas Meijer <bas.meijer@me.com>
2017-05-27 13:32:36 +0200

    update 2017

--- defaults.sh
+++ defaults.sh
@@ -105,9 +105,5 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 ###############################################################################
 
 # Only use UTF-8 in Terminal.app
-#defaults write com.apple.terminal StringEncodings -array 4
+defaults write com.apple.terminal StringEncodings -array 4
 
-# Enable “focus follows mouse” for Terminal.app and all X11 apps
-# i.e. hover over a window and start typing in it without clicking first
-#defaults write com.apple.terminal FocusFollowsMouse -bool true
-#defaults write org.x.X11 wm_ffm -bool true
--- install_on_my_mac.sh
+++ install_on_my_mac.sh
@@ -7,11 +7,11 @@
 ## download_apple_xcode: http://appstore.com
 # You need to download XCode, or only the command line tools
 xcrun -f git || (open https://developer.apple.com/xcode/downloads/ && exit 1)
-    
+
 ## install_apple_xcode_command_line_tools:
 # then you can install the command line tools you need
 xcrun -f git ||sudo xcodebuild -license
-xcrun -f git ||xcode-select --install 
+xcrun -f git ||xcode-select --install
 
 # To avoid pre-heartbleed SSL certificate errors boostrap without check
 echo insecure > ~/.curlrc
@@ -20,24 +20,24 @@ echo insecure > ~/.curlrc
 # Brew is the package manager of choice on the Mac
 if [ ! -x /usr/local/bin/brew ]
 then
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
+   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
 fi
 
 ## install_ansible: http://ansible.com
 brew install --update ansible
 
 ## install_cask: http://caskroom.io
-brew install --update caskroom/cask/brew-cask
+#brew install --update caskroom/cask/brew-cask
 
 ##install_from_galaxy:
 # re-use the role from 'Ansible for DevOps, Jeff Geerling, 2014'
-ansible-galaxy install --force -r requirements.yml
+ansible-galaxy install -p roles --force -r requirements.yml
 
 ##install_software_on_this_mac:
 # specify the apps you want in vars/main.yml
 ansible-playbook provisioner.yml -i inventory.ini --ask-sudo-pass
 
-# To avoid post-heartbleed SSL certificate errors use default curl 
+# To avoid post-heartbleed SSL certificate errors use default curl
 rm ~/.curlrc
 
 # Update this Mac immediately!
